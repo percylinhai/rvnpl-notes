7782c93a247f751b3163afb2aed619b35a6c37e7
Gao Wang <gaow@uchicago.edu>
2019-09-30 23:57:32 -0500

    Update individual gene analysis notebook

--- command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-rvnpl npl --path /mnt/large_ped_files/whole_exome_NFE_Dutch_filtered_AN_DP_GQ_BOTHpass_missrateByCenter_func_gnomAD_rvhaplo_chr11_allmaf_v0420_v2/SORL1 --exact --info_only --rvibd --n_jobs 8 -c 0.1 --fam_rep 10000 --perfect_max 1000
