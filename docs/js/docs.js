e0087803aaaebc0d3bbf4a476c60690eef03818a
Linhai Zhao <percylinhai@gmail.com>
2019-12-03 08:51:22 -0600

    update website

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,3 +1,3 @@
-var analysisArray = ['20190930_NPL_Gene_Analysis']
-var analysisDict = {'RV-NPL-analysis-for-selected-genes-1': '20190930_NPL_Gene_Analysis'}
-var analysisArrayMap = {'20190930_NPL_Gene_Analysis': 'RV-NPL analysis for selected genes'}
+var analysisArray = ['20190930_NPL_Gene_Analysis', 'NPL_Simulation']
+var analysisDict = {'RV-NPL-analysis-for-selected-genes-in-ADSP-pedigrees-1': '20190930_NPL_Gene_Analysis', 'RV-NPL-analysis-for-simulated-genes-1': 'NPL_Simulation'}
+var analysisArrayMap = {'20190930_NPL_Gene_Analysis': 'RV-NPL analysis for sel ... SP pedigrees', 'NPL_Simulation': 'RV-NPL analysis for simulated genes'}
