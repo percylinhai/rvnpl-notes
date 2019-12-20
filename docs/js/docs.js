7a93ec1d440a1fb0b21c4d601bef532278097135
Percy Zhao <percylinhai@gmail.com>
2019-12-20 13:56:50 -0600

    update html

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,3 +1,3 @@
-var analysisArray = ['20190930_NPL_Gene_Analysis', 'NPL_Simulation']
-var analysisDict = {'RV-NPL-analysis-for-selected-genes-in-ADSP-pedigrees-1': '20190930_NPL_Gene_Analysis', 'RV-NPL-analysis-for-simulated-genes-1': 'NPL_Simulation'}
-var analysisArrayMap = {'20190930_NPL_Gene_Analysis': 'RV-NPL analysis for sel ... SP pedigrees', 'NPL_Simulation': 'RV-NPL analysis for simulated genes'}
+var analysisArray = ['NPL_Simulation']
+var analysisDict = {'RV-NPL-analysis-for-simulated-genes-1': 'NPL_Simulation'}
+var analysisArrayMap = {'NPL_Simulation': 'RV-NPL analysis for simulated genes'}
