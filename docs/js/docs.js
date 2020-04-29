6a54f962f24ee90691c13a2fe61c29a45a2da65b
Gao Wang <wangow@gmail.com>
2020-04-29 16:22:35 -0400

    Add QNPL HTML docs

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,3 +1,3 @@
-var analysisArray = ['NPL_Simulation']
-var analysisDict = {'RV-NPL-analysis-for-simulated-genes-1': 'NPL_Simulation'}
-var analysisArrayMap = {'NPL_Simulation': 'RV-NPL analysis for simulated genes'}
+var analysisArray = ['NPL_Simulation', 'QNPL_Simulation']
+var analysisDict = {'RV-NPL-analysis-for-simulated-genes-1': 'NPL_Simulation', 'RV-QNPL-analysis-for-simulated-genes-1': 'QNPL_Simulation'}
+var analysisArrayMap = {'NPL_Simulation': 'RV-NPL analysis for simulated genes', 'QNPL_Simulation': 'RV-QNPL analysis for simulated genes'}
