7843f19fc0e1cc7d8c7a9fa6df05ee152717a0bf
Gao Wang <wangow@gmail.com>
2020-05-01 21:28:32 -0400

    Update documentation

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,3 +1,3 @@
 var analysisArray = ['NPL_Simulation', 'QNPL_Simulation']
-var analysisDict = {'RV-NPL-analysis-for-simulated-genes-1': 'NPL_Simulation', 'RV-QNPL-analysis-for-simulated-genes-1': 'QNPL_Simulation'}
-var analysisArrayMap = {'NPL_Simulation': 'RV-NPL analysis for simulated genes', 'QNPL_Simulation': 'RV-QNPL analysis for simulated genes'}
+var analysisDict = {'RV-NPL-analysis-for-simulated-genes-1': 'NPL_Simulation', 'Evaluation-of-RV-QNPL-Through-Simulation-Studies-1': 'QNPL_Simulation'}
+var analysisArrayMap = {'NPL_Simulation': 'RV-NPL analysis for simulated genes', 'QNPL_Simulation': 'Evaluation of RV-QNPL T ... tion Studies'}
